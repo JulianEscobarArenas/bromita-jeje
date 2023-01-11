08b4c68ecde78ddc7e731e808174a0deec434f78
JulianEscobarArenas <105441409+JulianEscobarArenas@users.noreply.github.com>
2023-01-10 20:18:33 -0500

    Add files via upload

---
+++ hack_bulloso.bat
@@ -0,0 +1,13 @@
+start calc
+start notepad
+start calc
+start notepad
+start mspaint
+start calc
+start notepad
+start mspaint
+start calc
+start notepad
+start mspaint
+
+start https://academiaplay.es/wp-content/uploads/2018/01/troll-wil-hughes-troll-face.jpg
