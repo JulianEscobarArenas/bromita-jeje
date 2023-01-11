8d9fcb8a0270f9fb4042e4897efde816a81d7502
JulianEscobarArenas <105441409+JulianEscobarArenas@users.noreply.github.com>
2023-01-10 20:24:59 -0500

    Add files via upload

---
+++ hack mega alborotado.bat
@@ -0,0 +1,11 @@
+@echo off
+
+color a
+
+:top
+
+echo %random%%random%%%random%%random%%%random%%random%%%random%%random%%%random%%random%%%random%%random%%%random%%random%%
+
+goto top
+
+start http://img.desmotivaciones.es/201011/BlackTrollFace.jpg
